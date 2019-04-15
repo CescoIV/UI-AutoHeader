d8fc274dc98b3a7d74b6d4f0fc82d75183c6b059
Cesco <crordaz@gmail.com>
2019-04-14 19:00:39 -0500

    data structure planning

---
+++ header.js
@@ -0,0 +1,14 @@
+/*
+
+Example base objects to recieve
+
+{
+    headers: ["strs"...],
+    hasSubMenu: false,
+    subMenus: [["i","have","menus"],[]],
+    rgbaColor: "rgba(0,0,0,0)",
+    
+
+}
+*/
+
